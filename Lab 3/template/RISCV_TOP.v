d153e8a9840d6d721c32427568be28324012e0aa
diuni <wldbs0507@kaist.ac.kr>
2020-10-04 15:25:32 +0900

    opcode case division

--- Lab 3/template/RISCV_TOP.v
+++ Lab 3/template/RISCV_TOP.v
@@ -25,6 +25,7 @@ module RISCV_TOP (
 	input wire [31:0] RF_RD2,
 	output wire [31:0] RF_WD,
 	output wire HALT,                   // if set, terminate program
+	// If get the instruction sequences 0x00c00093 0x00008067, HALT output wire should be set to 1
 	output reg [31:0] NUM_INST,         // number of instruction completed
 	output wire [31:0] OUTPUT_PORT      // equal RF_WD this port is used for test
 	);
@@ -41,5 +42,45 @@ module RISCV_TOP (
 	end
 
 	// TODO: implement
+	// PC(D_MEM_ADDR??? idk), for HALT
+	wire PRE_HALT;
+	initial begin
+		D_MEM_ADDR <= 0;
+		PRE_HALT <= 0;
+	end
 
+	// does it cover also in sequentially same NUM_INST?
+	always @ (NUM_INST) begin
+		if (NUM_INST == 0x00c00093) begin
+			PRE_HALT = 1;
+		end 
+		else begin
+			PRE_HALT = 0;
+		end
+		if (PRE_HALT == 1 && NUM_INST == 0x00008067) begin
+			PRE_HALT = 0;
+			HALT = 1;
+		end
+		case (NUM_INST[6:0])
+			// LUI
+			// WHy do we have to write 7b' prefix?
+			7b'0110111 :
+			// AUIPC
+			7b'0010111 :
+			// JAL
+			7b'1101111 :
+			// JALR
+			7b'1100111 :
+			// B(BRANCH) Type (BEQ, BNE, BLT, BGE, BLTU, BGEU)
+			7b'1100011 :
+			// I Type Load (LB, LH, LW, LBU, LHU)
+			7b'0000011 :
+			// Store (SB, SH, SW)
+			7b'0100011 :
+			// I Type (ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI)
+			7b'0010011 :
+			// R Type (ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND)
+			7b'0110011 :
+			
+	end
 endmodule //
