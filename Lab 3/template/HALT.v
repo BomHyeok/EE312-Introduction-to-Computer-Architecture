cdd805ddc3261d4983ec3ed64c1d1be58aff46a2
diuni <wldbs0507@kaist.ac.kr>
2020-10-08 00:55:26 +0900

    Update Halt

--- Lab 3/template/HALT.v
+++ Lab 3/template/HALT.v
@@ -5,7 +5,7 @@ module HALT(
 
     reg _HALT, PRE_HALT;
     assign HALT = _HALT;
-    
+
     initial begin
         _HALT = 0;
         PRE_HALT = 0;
@@ -15,5 +15,6 @@ module HALT(
         if (INSTR == 32'h00c00093) PRE_HALT = 1;
         else PRE_HALT = 0;
         if (PRE_HALT == 1 && INSTR == 32'h00008067) _HALT = 1;
+        else PRE_HALT = 0;
     end
 endmodule
