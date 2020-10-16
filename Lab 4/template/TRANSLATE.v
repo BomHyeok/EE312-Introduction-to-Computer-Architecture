20dee4b80cb02bdb7b56165ed70df8c51d5aa52c
diuni <wldbs0507@kaist.ac.kr>
2020-10-17 01:12:32 +0900

    Upload from Lab 3

---
+++ Lab 4/template/TRANSLATE.v
@@ -0,0 +1,15 @@
+module TRANSLATE(
+    input wire [31:0] EFFECTIVE_ADDR,
+    input wire IorD,
+    output wire [11:0] MEM_ADDR
+    );
+
+    reg [11:0] TEMP_ADDR;
+    assign MEM_ADDR = TEMP_ADDR;
+    initial TEMP_ADDR = 0;
+    
+    always @ (*) begin
+        if (IorD == 0) TEMP_ADDR = EFFECTIVE_ADDR[11:0] & 12'hfff;
+        else TEMP_ADDR = EFFECTIVE_ADDR[14:0] & 15'h3fff;
+    end
+endmodule
