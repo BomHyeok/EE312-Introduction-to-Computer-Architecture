bd2d3ef9c3b8ae479d2a9246a02d49c33d43b4ee
BomHyeok <59109668+BomHyeok@users.noreply.github.com>
2020-11-14 18:55:52 +0900

    Update PR_IDEX.v

--- Lab 5/template/PR_IDEX.v
+++ Lab 5/template/PR_IDEX.v
@@ -1,6 +1,6 @@
 module PR_IDEX(
 	input wire CLK,
-
+	input wire RSTn,
 	// IMM & Register
 	input wire [31:0] IMM,
 	input wire [4:0] RF_RA1, RF_RA2, RF_WA1,
@@ -54,25 +54,44 @@ module PR_IDEX(
 	assign RWSrc_IDEX = RWSrc_TEMP;
 	assign RF_WE_IDEX = RF_WE_TEMP;
 
+	initial begin
+		IMM_TEMP = 0;
+		RF_RA1_TEMP = 0;
+ 		RF_RA2_TEMP = 0;
+		RF_WA1_TEMP = 0;
+		RF_RD1_TEMP = 0;
+		RF_RD2_TEMP = 0;
+		ALUOp_TEMP = 0;
+		ALUSrcA_TEMP = 0;
+		ALUSrcB_TEMP = 0;
+		D_MEM_BE_TEMP = 0;
+		D_MEM_WEN_TEMP = 0;
+		D_MemRead_TEMP = 0;
+		RWSrc_TEMP = 0;
+		RF_WE_TEMP = 0;
+	end
+
 	always @ (negedge CLK) begin
-		IMM_TEMP = IMM;
-		RF_RA1_TEMP = RF_RA1;
-		RF_RA2_TEMP = RF_RA2;
-		RF_WA1_TEMP = RF_WA1;
+		if (RSTn) begin
+			IMM_TEMP = IMM;
+			RF_RA1_TEMP = RF_RA1;
+			RF_RA2_TEMP = RF_RA2;
+			RF_WA1_TEMP = RF_WA1;
 		
-		RF_RD1_TEMP = RF_RD1;
-		RF_RD2_TEMP = RF_RD2;
+			RF_RD1_TEMP = RF_RD1;
+			RF_RD2_TEMP = RF_RD2;
 
-		ALUOp_TEMP = ALUOp_IFID;
-		ALUSrcA_TEMP = ALUSrcA_IFID;
-		ALUSrcB_TEMP = ALUSrcB_IFID;
+			ALUOp_TEMP = ALUOp_IFID;
+			ALUSrcA_TEMP = ALUSrcA_IFID;
+			ALUSrcB_TEMP = ALUSrcB_IFID;
 
-		D_MEM_BE_TEMP = D_MEM_BE_IFID;
-		D_MEM_WEN_TEMP = D_MEM_WEN_IFID;
-		D_MemRead_TEMP = D_MemRead_IFID;
-	
-		RWSrc_TEMP = RWSrc_IFID;
-		RF_WE_TEMP = RF_WE_IFID;
+			D_MEM_BE_TEMP = D_MEM_BE_IFID;
+			D_MEM_WEN_TEMP = D_MEM_WEN_IFID;
+			D_MemRead_TEMP = D_MemRead_IFID;
+		
+			RWSrc_TEMP = RWSrc_IFID;
+			RF_WE_TEMP = RF_WE_IFID;
+		end
 	end
 		
 
