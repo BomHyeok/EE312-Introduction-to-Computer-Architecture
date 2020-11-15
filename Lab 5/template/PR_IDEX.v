78bf5f26b779f6f0f2c85884631464fb7c6207d2
BomHyeok <59109668+BomHyeok@users.noreply.github.com>
2020-11-15 14:49:31 +0900

    Update PR_IDEX.v

--- Lab 5/template/PR_IDEX.v
+++ Lab 5/template/PR_IDEX.v
@@ -1,27 +1,29 @@
 module PR_IDEX(
 	input wire CLK,
 	input wire RSTn,
+	input wire [31:0] ADD_PC,
 	// IMM & Register
 	input wire [31:0] IMM,
 	input wire [4:0] RF_RA1, RF_RA2, RF_WA1,
 	input wire [31:0] RF_RD1, RF_RD2,
 	// EX
 	input wire [3:0] ALUOp_IFID, 
-	input wire ALUSrcA_IFID, ALUSrcB_IFID,
+	input wire ALUSrcA_IFID, ALUSrcB_IFID, isJump_IFID, isLoad_IFID,
 	// MEM
 	input wire [3:0] D_MEM_BE_IFID, 
 	input wire D_MEM_WEN_IFID, D_MemRead_IFID,
 	// WB
 	input wire [1:0] RWSrc_IFID,
 	input wire RF_WE_IFID,
-
+	
+	output wire [31:0] ADD_PC_IDEX,
 	// IMM & Register
 	output wire [31:0] IMM_OUT,
 	output wire [4:0] RF_RA1_OUT, RF_RA2_OUT, WA_IDEX,
 	output wire [31:0] RF_RD1_OUT, RF_RD2_OUT,
 	// EX
 	output wire [3:0] ALUOp, 
-	output wire ALUSrcA, ALUSrcB,
+	output wire ALUSrcA, ALUSrcB, isJump, isLoad,
 	// MEM
 	output wire [3:0] D_MEM_BE_IDEX, 
 	output wire D_MEM_WEN_IDEX, D_MemRead_IDEX,
@@ -30,12 +32,13 @@ module PR_IDEX(
 	output wire RF_WE_IDEX
 );
 	
-	reg [31:0] IMM_TEMP, RF_RD1_TEMP, RF_RD2_TEMP;
+	reg [31:0] ADD_PC_TEMP, IMM_TEMP, RF_RD1_TEMP, RF_RD2_TEMP;
 	reg [4:0] RF_WA1_TEMP, RF_RA1_TEMP, RF_RA2_TEMP;
 	reg [3:0] ALUOp_TEMP, D_MEM_BE_TEMP;
 	reg [1:0] RWSrc_TEMP;
-	reg ALUSrcA_TEMP, ALUSrcB_TEMP, D_MEM_WEN_TEMP, D_MemRead_TEMP, RF_WE_TEMP;
+	reg ALUSrcA_TEMP, ALUSrcB_TEMP, D_MEM_WEN_TEMP, D_MemRead_TEMP, RF_WE_TEMP, isJump_TEMP, isLoad_TEMP;
 
+	assign ADD_PC_IDEX = ADD_PC_TEMP;
 	assign IMM_OUT = IMM_TEMP;
 	assign RF_RA1_OUT = RF_RA1_TEMP;
  	assign RF_RA2_OUT = RF_RA2_TEMP;
@@ -46,6 +49,8 @@ module PR_IDEX(
 	assign ALUOp = ALUOp_TEMP;
 	assign ALUSrcA = ALUSrcA_TEMP;
 	assign ALUSrcB = ALUSrcB_TEMP;
+	assign isJump = isJump_TEMP;
+	assign isLoad = isLoad_TEMP;
 
 	assign D_MEM_BE_IDEX = D_MEM_BE_TEMP;
 	assign D_MEM_WEN_IDEX = D_MEM_WEN_TEMP;
@@ -55,6 +60,7 @@ module PR_IDEX(
 	assign RF_WE_IDEX = RF_WE_TEMP;
 
 	initial begin
+		ADD_PC_TEMP = 0;
 		IMM_TEMP = 0;
 		RF_RA1_TEMP = 0;
  		RF_RA2_TEMP = 0;
@@ -64,6 +70,8 @@ module PR_IDEX(
 		ALUOp_TEMP = 0;
 		ALUSrcA_TEMP = 0;
 		ALUSrcB_TEMP = 0;
+		isJump_TEMP = 0;
+		isLoad_TEMP = 0;
 		D_MEM_BE_TEMP = 0;
 		D_MEM_WEN_TEMP = 0;
 		D_MemRead_TEMP = 0;
@@ -73,6 +81,7 @@ module PR_IDEX(
 
 	always @ (negedge CLK) begin
 		if (RSTn) begin
+			ADD_PC_TEMP = ADD_PC;
 			IMM_TEMP = IMM;
 			RF_RA1_TEMP = RF_RA1;
 			RF_RA2_TEMP = RF_RA2;
@@ -84,6 +93,8 @@ module PR_IDEX(
 			ALUOp_TEMP = ALUOp_IFID;
 			ALUSrcA_TEMP = ALUSrcA_IFID;
 			ALUSrcB_TEMP = ALUSrcB_IFID;
+			isJump_TEMP = isJump_IFID;
+			isLoad_TEMP = isLoad_IFID;
 
 			D_MEM_BE_TEMP = D_MEM_BE_IFID;
 			D_MEM_WEN_TEMP = D_MEM_WEN_IFID;
