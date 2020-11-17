cffa725526f60fd929598fb697b917d46b8067d3
BomHyeok <59109668+BomHyeok@users.noreply.github.com>
2020-11-17 22:43:14 +0900

    Update PR_IDEX.v

--- Lab 5/template/PR_IDEX.v
+++ Lab 5/template/PR_IDEX.v
@@ -99,56 +99,56 @@ module PR_IDEX(
 	always @ (posedge CLK) begin
 		if (RSTn) begin
 			if (~FLUSH_IDEX) begin
-				PC_TEMP = PC_IFID;
-				ADD_PC_TEMP = ADD_PC_IFID;
-				HALT_IDEX_TEMP = HALT_IFID;
-				IMM_TEMP = IMM;
-				RF_RA1_TEMP = RF_RA1;
-				RF_RA2_TEMP = RF_RA2;
-				RF_WA1_TEMP = RF_WA1;
+				PC_TEMP <= PC_IFID;
+				ADD_PC_TEMP <= ADD_PC_IFID;
+				HALT_IDEX_TEMP <= HALT_IFID;
+				IMM_TEMP <= IMM;
+				RF_RA1_TEMP <= RF_RA1;
+				RF_RA2_TEMP <= RF_RA2;
+				RF_WA1_TEMP <= RF_WA1;
 			
-				RF_RD1_TEMP = RF_RD1;
-				RF_RD2_TEMP = RF_RD2;
+				RF_RD1_TEMP <= RF_RD1;
+				RF_RD2_TEMP <= RF_RD2;
 
-				ALUOp_TEMP = ALUOp_IFID;
-				ALUSrcA_TEMP = ALUSrcA_IFID;
-				ALUSrcB_TEMP = ALUSrcB_IFID;
+				ALUOp_TEMP <= ALUOp_IFID;
+				ALUSrcA_TEMP <= ALUSrcA_IFID;
+				ALUSrcB_TEMP <= ALUSrcB_IFID;
 
-				D_MEM_BE_TEMP = D_MEM_BE_IFID;
-				D_MEM_WEN_TEMP = D_MEM_WEN_IFID;
-				D_MemRead_TEMP = D_MemRead_IFID;
-				PCSrc_TEMP = PCSrc_IFID;
-				isJump_TEMP = isJump_IFID;
-				isLoad_TEMP = isLoad_IFID;
+				D_MEM_BE_TEMP <= D_MEM_BE_IFID;
+				D_MEM_WEN_TEMP <= D_MEM_WEN_IFID;
+				D_MemRead_TEMP <= D_MemRead_IFID;
+				PCSrc_TEMP <= PCSrc_IFID;
+				isJump_TEMP <= isJump_IFID;
+				isLoad_TEMP <= isLoad_IFID;
 			
-				RWSrc_TEMP = RWSrc_IFID;
-				OPSrc_TEMP = OPSrc_IFID;
-				RF_WE_TEMP = RF_WE_IFID;
-				NUM_CHECK_TEMP = NUM_CHECK_IFID;
+				RWSrc_TEMP <= RWSrc_IFID;
+				OPSrc_TEMP <= OPSrc_IFID;
+				RF_WE_TEMP <= RF_WE_IFID;
+				NUM_CHECK_TEMP <= NUM_CHECK_IFID;
 			end
 			else begin
-				PC_TEMP = 0;
-				ADD_PC_TEMP = 0;
-				HALT_IDEX_TEMP = 0;
-				IMM_TEMP = 0;
-				RF_RA1_TEMP = 0;
-				RF_RA2_TEMP = 0;
-				RF_WA1_TEMP = 0;
-				RF_RD1_TEMP = 0;
-				RF_RD2_TEMP = 0;
-				ALUOp_TEMP = 0;
-				ALUSrcA_TEMP = 0;
-				ALUSrcB_TEMP = 0;
-				D_MEM_BE_TEMP = 0;
-				D_MEM_WEN_TEMP = 0;
-				D_MemRead_TEMP = 0;
-				PCSrc_TEMP = 0;
-				isJump_TEMP = 0;
-				isLoad_TEMP = 0;
-				RWSrc_TEMP = 0;
-				OPSrc_TEMP = 0;
-				RF_WE_TEMP = 0;
-				NUM_CHECK_TEMP = 0;
+				PC_TEMP <= 0;
+				ADD_PC_TEMP <= 0;
+				HALT_IDEX_TEMP <= 0;
+				IMM_TEMP <= 0;
+				RF_RA1_TEMP <= 0;
+				RF_RA2_TEMP <= 0;
+				RF_WA1_TEMP <= 0;
+				RF_RD1_TEMP <= 0;
+				RF_RD2_TEMP <= 0;
+				ALUOp_TEMP <= 0;
+				ALUSrcA_TEMP <= 0;
+				ALUSrcB_TEMP <= 0;
+				D_MEM_BE_TEMP <= 0;
+				D_MEM_WEN_TEMP <= 0;
+				D_MemRead_TEMP <= 0;
+				PCSrc_TEMP <= 0;
+				isJump_TEMP <= 0;
+				isLoad_TEMP <= 0;
+				RWSrc_TEMP <= 0;
+				OPSrc_TEMP <= 0;
+				RF_WE_TEMP <= 0;
+				NUM_CHECK_TEMP <= 0;
 			end		
 		end
 	end
